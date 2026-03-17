5f847958240185fc2374d86b2e85954484d933ef
moreirabolfer <henriquebolfer1@hotmail.com>
2026-03-16 22:47:03 -0300

    aula 2

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,29 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # CONCATENA STRINGS
+
+# Comentarios de 1 linha no python
+
+'''
+    Comentarios de
+    multiplas
+    linhas
+    '''
+
+# VARIAVEIS
+
+nome = "moreira"
+idade = 18
+peso = 60
+
+print(nome, idade, peso)
+
+# INPUT - SIMULAR UM FORMULARIO NO CMD
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
+print(nome, idade, peso)
+print(f'Oiiii, {nome}')
