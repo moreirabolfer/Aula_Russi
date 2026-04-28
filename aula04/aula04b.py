5c9e2792a68477e1fbdb29fbfca334baefd09756
moreirabolfer <henriquebolfer1@hotmail.com>
2026-04-27 22:29:45 -0300

    aula04

---
+++ aula04/aula04b.py
@@ -0,0 +1,16 @@
+# for count_music in range(3):
+#     print(f"Musica {count_music}")
+#
+# # de 1 até 11, pulando de 2 em 2
+# for i in range(1, 12, 2):
+#     print(i)
+
+# ATV 3
+# qtd_musicas = int(input("Digite a qtd de musicas da sua playlist (DB): "))
+#
+# for i in range(qtd_musicas):
+#     print(f"Musica {i}")
+
+for i in range(0, 4):
+    for j in range(0, 3, 2):
+        print(f"i:{i}, j:{j}")
